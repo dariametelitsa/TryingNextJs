2c3983e2c775d298c7944aba79f3f6e85efc4bcf
Daria Metelitsa <dariametelitsa@gmail.com>
2024-08-30 20:24:24 +0300

    added button styles

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,4 +1,6 @@
 import React, { ComponentProps } from "react";
+import { cn } from "@/utils/cn";
+import { cva, VariantProps } from "class-variance-authority";
 
 // type Button = {
 //     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -6,14 +8,50 @@ import React, { ComponentProps } from "react";
 //     children: ReactNode
 //  }
 
-type Button = ComponentProps<'button'> & {
-    variant?: 'primary' | 'secondary' | 'outlined' | 'text'
-}
+const buttonVariants = cva(["rounded inline-flex px-3 py-2 text-slate-50"], { //дефолтные стили
+    variants: {
+        variant: {
+            primary: [
+                "bg-accent-500",
+                "hover:bg-accent-100",
+                "active:bg-accent-700"
+            ],
+            secondary: [
+                "bg-gray-500",
+                "hover:bg-gray-600",
+                "active: bg-[#212121]"
+            ],
+        },
+    },
+    defaultVariants: {
+        variant: "primary",
+    },
+});
+
+// type Button = ComponentProps<'button'> & {
+//     variant?: 'primary' | 'secondary' | 'outlined' | 'text'
+// }
+
+type Button = ComponentProps<'button'> & VariantProps<typeof buttonVariants>
 
 function Button({variant = 'primary', className, ...props}: Button) {
     return (
-        <button className={`rounded inline-flex px-3 py-2 ${variant === 'primary' ? 'bg-blue-500 text-slate-50' : ''}
-        ${variant === 'secondary' ? 'bg-cyan-800 text-slate-50' : ''}`} {...props}>{props.children}</button>
+        ////first
+        // <button className={`rounded inline-flex px-3 py-2 ${variant === 'primary' ? 'bg-blue-500 text-slate-50' : ''}
+        // ${variant === 'secondary' ? 'bg-cyan-800 text-slate-50' : ''}`} {...props}>{props.children}</button>
+
+        // //second
+        // <button className={cn(`rounded inline-flex px-3 py-2 text-slate-50`,
+        //     variant === 'primary' && cn('bg-blue-500', 'hover:bg-blue-400'),
+        //     variant === "secondary" && 'bg-cyan-800',
+        //     className)
+        // } {...props}>{props.children}</button>
+
+        //third
+        <button className={cn(buttonVariants({variant}), className)}>{props.children}</button>
+
+
     );
 }
+
 export { Button }
