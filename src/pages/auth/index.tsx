b19aa70685b0dd5e46e6536c9df317b0b127072a
Daria Metelitsa <dariametelitsa@gmail.com>
2024-08-30 18:59:16 +0300

    initial commit

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+
+export default function auth (props: Props) {
+    return (
+        <div>
+            auth
+        </div>
+    );
+};
+
