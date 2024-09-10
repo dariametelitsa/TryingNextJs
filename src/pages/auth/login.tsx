a4dfdd8132b16c95abe800a758c08f0641357f03
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-10 17:23:45 +0300

    refactor: change logic for login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,33 +1,48 @@
-import { useState, FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const emailRegex = new RegExp(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim);
+type LoginFields = {
+    email: string
+    password: string
+}
 
 export default function Login() {
+    const {handleSubmit, register, formState: {errors}} = useForm<LoginFields>();
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+    })
 
-    const [emailError, setEmailError] = useState<string | null>(null)
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const formObj = Object.fromEntries(new FormData(e.currentTarget));
-        console.log(formObj);
-    }
+    // const [emailError, setEmailError] = useState<string | null>(null)
+    // const _handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     const formObj = Object.fromEntries(new FormData(e.currentTarget));
+    //     console.log(formObj);
+    // }
 
     return (
         <div className={'h-screen grid place-items-center'}>
-            <form onSubmit={handleSubmit} className={'space-y-10'}>
+            <form onSubmit={onSubmit} className={'space-y-10'}>
                 <div className={'flex flex-col gap-1'}>
                     <label htmlFor={'email-input'}>Email</label>
                     <input id={'email-input'}
-                           name={'email'}
-                           type={'email'}
+                           {...register('email', {
+                             pattern: {value: emailRegex, message: 'Please enter a valid email'},
+                             required: 'Email is required',
+                           })}
+                           // type={'email'}
                            placeholder={'Email'}/>
-                    {emailError && <p className={'text-red-500 text-sm'}>{emailError}</p>}
+                    {errors.email && <p className={'text-red-500 text-sm'}>{errors.email.message}</p>}
                 </div>
                 <div className={'flex flex-col gap-2'}>
                     <label htmlFor={'password-input'}>Password</label>
                     <input id={'password-input'}
-                           name={'password'}
+                           {...register('password', {
+                               minLength: {value: 3, message: 'Password has to be longer than 3 characters'},
+                               required: 'Password is required',
+                           })}
                            placeholder={'Password'}/>
+                    {errors.password && <p className={'text-red-500 text-sm'}>{errors.password.message}</p>}
                 </div>
 
                 <button>Sign In</button>
